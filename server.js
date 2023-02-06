bb1b21cfcba130b3af01dc82512a00ea98a4563f
dogpalse <dogpalse@gmail.com>
2023-02-06 11:26:09 +0900

    add index.html

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -6,7 +7,8 @@ const PORT = process.env.PORT || 8000;
 app.set('port', PORT);
 
 app.get('/', (req, res) => {
-    res.send('Welcome Node Server...!');
+    // res.send('Welcome Node Server...!');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(app.get('port'), () => {
