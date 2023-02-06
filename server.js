def0c9dd1121c751412b9e34c2bee4abca92b1dd
dogpalse <dogpalse@gmail.com>
2023-02-06 14:11:26 +0900

    add public dir and revise .gitignore

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const path = require('path');
+const dotenv = require('dotenv');
+const logger = require('morgan');
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.set('port', PORT);
+
+// Logger Middleware
+// [HTTP Method] [URL] [HTTP Status] [Response Rate] - [Response Byte]
+app.use(logger('dev'));
 
 // Sample Middleware
 app.use((req, res, next) => {
     console.log('Middleware at all request');
     next();
 });
+
+// Static Files Supply Middleware
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+// Body-Parser Middleware
+// Form Data or AJAX Request to req.body Object
+app.use(express.json());                                // JSON 형식으로 데이터 전달
+// extended가 false = node의 querystring 모듈 사용
+// extended가 true = qs 모듈 사용
+app.use(express.urlencoded({ extended: false }));       // 주소 형식(Ex. Form Data)으로 데이터 전달
 
 app.get('/', (req, res, next) => {
     // res.send('Welcome Node Server...!');
